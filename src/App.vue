0fefa21ff776f039389269d641d82d6fe221c260
Joshua O'Sullivan <jrosullivan1@sheffield.ac.uk>
2020-05-07 12:53:03 +0100

    Implement spotify authentication

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a v-if="!authorised" :href="authorizeURL">Authenticate</a>
+    <p v-else>{{ token }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+var SpotifyWebApi = require('spotify-web-api-node');
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  mounted() {
+    console.log(this.$route)
+  },
+  computed: {
+    authorised() {
+      return true
+    },
+    token() {
+      return this.$route.query.code
+    },
+    authorizeURL() {
+      var scopes = ['user-library-read', 'user-modify-playback-state']
+      var redirectUri = location.href
+      var clientId = '8c00d93547824017b1854018ed35bdef'
+
+      console.log(redirectUri)
+
+      var spotifyApi = new SpotifyWebApi({
+        redirectUri: redirectUri,
+        clientId: clientId
+      });
+
+      return spotifyApi.createAuthorizeURL(scopes);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+var router = new VueRouter({
+  mode: 'history',
+  routes: []
+});
+
+import Spotify from 'spotify-web-api-node'
+import VueSpotify from 'vue-spotify'
+Vue.use(VueSpotify, new Spotify())
+
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
