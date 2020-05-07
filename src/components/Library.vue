dcb05b3cb602ea9ebc3797b99eef73019c097e10
Joshua O'Sullivan <jrosullivan1@sheffield.ac.uk>
2020-05-07 22:14:15 +0100

    Add user's display name to header + link (closes #2)

--- src/components/Library.vue
+++ src/components/Library.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="library">
-      <h1>Your saved albums, by year!</h1>
-      <ol v-if="loaded">
+      <h1 v-if="profile">
+        <a :href="profile.external_urls.spotify" class="username"
+          >{{ profile.display_name }}</a>{{ profile.display_name == 's' ? '\'' : '\'s' }}
+          saved albums, by year<span>!</span>
+        </h1>
+      <ol v-if="albumsByYear">
         <Year v-for="year in sortedAlbumYears" :key="year"
         :year="year"
         :albums="albumsByYear[year].albums"
@@ -22,16 +26,20 @@ export default {
   props: ["access_token"],
 
   data() { return {
-      albumsByYear: {},
-      loaded: false
+      albumsByYear: null,
+      profile: null,
   }},
   
   computed: {
-    request_url() {
+    albums_request_url() {
       return "https://api.spotify.com/v1/me/albums?" +
       "access_token=" + this.access_token +
       "&limit=50" +
       "&offset=0"
+    },
+    profile_request_url() {
+      return "https://api.spotify.com/v1/me/?" +
+      "access_token=" + this.access_token
     },
     sortedAlbumYears() {
       return Object.keys(this.albumsByYear).sort((a,b)=>{return b-a})
@@ -39,18 +47,22 @@ export default {
   },
 
   mounted() {
-    console.log(this.request_url)
-    axios.get(this.request_url)
+    axios.get(this.albums_request_url)
       .then(
         function(data) {
           data = data["data"]["items"]
+          this.albumsByYear = {}
           for (var album of data) {
             const releaseYear = album["album"]["release_date"].split("-")[0]
             this.albumsByYear[releaseYear] ? null : this.albumsByYear[releaseYear] = { year: releaseYear, albums: [] }
             this.albumsByYear[releaseYear]["albums"].push(album)
           }
-          this.loaded = true;
-          console.log(this.albumsByYear)
+        }.bind(this)
+      )
+    axios.get(this.profile_request_url)
+      .then(
+        function(data) {
+          this.profile = data.data
         }.bind(this)
       )
 
