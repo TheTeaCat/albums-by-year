cad518047dad2f27a8d6b3f7b7c8469f56a0ca86
Joshua O'Sullivan <jrosullivan1@sheffield.ac.uk>
2020-05-07 16:16:51 +0100

    Create list of year components

--- src/components/Library.vue
+++ src/components/Library.vue
@@ -1,20 +1,29 @@
 <template>
-  <div id="library">
-      {{ albums }}
+  <div class="library">
+      <ol v-if="loaded">
+        {{ sortedAlbumYears }}
+        <Year v-for="year in sortedAlbumYears" :key="year"
+        :year="year"
+        :albums="albumsByYear[year].albums"
+        :access_token="access_token" />
+      </ol>
   </div>
 </template>
 
 <script>
 import axios from "axios"
+import Year from "./Year.vue";
 
 export default {
   components: {
+    Year
   },
 
   props: ["access_token"],
 
   data() { return {
-      albums: null
+      albumsByYear: {},
+      loaded: false
   }},
   
   computed: {
@@ -23,16 +32,28 @@ export default {
       "access_token=" + this.access_token +
       "&limit=50" +
       "&offset=0"
+    },
+    sortedAlbumYears() {
+      return Object.keys(this.albumsByYear).sort((a,b)=>{return b-a})
     }
   },
 
   mounted() {
+    console.log(this.request_url)
     axios.get(this.request_url)
-         .then(
-           function(data) {
-             this.albums = data["data"]["items"]
-           }.bind(this)
-          )
+      .then(
+        function(data) {
+          data = data["data"]["items"]
+          for (var album of data) {
+            const releaseYear = album["album"]["release_date"].split("-")[0]
+            this.albumsByYear[releaseYear] ? null : this.albumsByYear[releaseYear] = { year: releaseYear, albums: [] }
+            this.albumsByYear[releaseYear]["albums"].push(album)
+          }
+          this.loaded = true;
+          console.log(this.albumsByYear)
+        }.bind(this)
+      )
+
   },
 }
 </script>
---
+++ src/components/Year.vue
@@ -0,0 +1,21 @@
+<template>
+  <li class="year">
+    <h2>{{ year }} ({{ albums.length }})</h2>
+    <ol>
+      {{ albums }}
+    </ol>
+  </li>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+
+  props: ["access_token","year","albums"],
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
